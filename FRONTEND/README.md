f3cacd133d5ffa5d84f810a88be8ef3e55814119
braedonb98 <brbe3574@colorado.edu>
2022-01-19 15:35:09 -0700

    added page not found redirect to home link

--- FRONTEND/README.md
+++ FRONTEND/README.md
@@ -1,3 +1,5 @@
 List Of know issues
 - app.js 
    -need to fix default routing to page not found page 
+-PageNotFound.js
+    -currently go back to our homepage is a hard link to local host 3000
--- FRONTEND/src/landing/pages/PageNotFound.js
+++ FRONTEND/src/landing/pages/PageNotFound.js
@@ -6,6 +6,9 @@ const PageNotFound = () => {
         <div>
           <h1>Sorry</h1>
           <h2>Something went wrong on our end</h2>
+          <h3>please go back and try again</h3>
+          <h3>or go to </h3>
+          <a href = "http://localhost:3000/"> Our Home Page</a>
         </div>
       </React.Fragment>
     )
