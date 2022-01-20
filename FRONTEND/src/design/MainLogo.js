e80f3c8d15f7198c0f88e9a9cf1bfb5740847141
braedonb98 <brbe3574@colorado.edu>
2022-01-19 23:41:39 -0700

    adding logo still needs to be added to nav bar

--- FRONTEND/src/design/MainLogo.js
+++ FRONTEND/src/design/MainLogo.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./MainLogo.css";
 const MainLogo = props => {
-    const logo ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaXlGmAvAggLWzHdcUAZx_rVB8Q1OGR_Nn4Q&usqp=CAU";
+    const logo ="./2DoFinanceLogo.png";
     return (
         <img className = {props.styling} src={logo} alt ="2 DO Finance"/>
     )
