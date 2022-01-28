4cff890bc7f6a968b7087cf1f01aebf5817b9b69
braedonb98 <brbe3574@colorado.edu>
2022-01-28 10:07:04 -0700

    removed form element from modal overlay because it caused a form inside a form which according to console is bad

--- FRONTEND/src/shared/components/UIElements/Modal.js
+++ FRONTEND/src/shared/components/UIElements/Modal.js
@@ -11,14 +11,14 @@ const ModalOverlay = props => {
             <header className = {`modal__header ${props.headerClass}`}>
                 <h2>{props.header}</h2>
             </header>
-            <form onSubmit = {props.onSubmit ? props.onSubmit : event => event.preventDefault()}>
+            
                 <div className ={`modal__content ${props.contentClass}`}>
                     {props.children}
                 </div>
                 <footer className={`modal__footer ${props.footerClass}`}>
                     {props.footer}
                 </footer>
-            </form>
+            
         </div>
     );
     return ReactDOM.createPortal(content, document.getElementById('modal-hook'));
