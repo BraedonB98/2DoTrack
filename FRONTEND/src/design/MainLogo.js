281f61f63377281c535fdb02b336c505ecf55fed
braedonb98 <brbe3574@colorado.edu>
2022-02-08 18:30:02 -0700

    readme

--- FRONTEND/src/design/MainLogo.js
+++ FRONTEND/src/design/MainLogo.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 import "./MainLogo.css";
-const MainLogo = props => {
-    return (
-        <img className = {props.styling} src={`${process.env.REACT_APP_ASSET_URL}/data/frontendref/images/2DoFinanceLogo.png`} alt ="2 DO Finance"/>
-    )
-}
+const MainLogo = (props) => {
+  return (
+    <img
+      className={props.styling}
+      src={`${process.env.REACT_APP_ASSET_URL}/data/frontendref/images/2DoTrackLogo.png`}
+      alt="2 DO Finance"
+    />
+  );
+};
 
 export default MainLogo;
